073a51875088cc447fd6332e474034fd6e8a9e10
kozoyatov <56381423+kozoyatov@users.noreply.github.com>
2020-06-04 18:01:27 +0300

    Add files via upload

---
+++ image_class.py
@@ -0,0 +1,8 @@
+from type_class import Type
+
+class Image:
+  def __init__(self, path,folderName):
+    self.path = path
+    self.folderName = folderName
+    self.data=[]
+    self.type = Type
---
+++ type_class.py
@@ -0,0 +1,6 @@
+import enum
+
+class Type(enum.Enum):
+  train = 1
+  test = 2
+
